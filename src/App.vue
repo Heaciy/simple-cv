dec863b5b7086589a971560d4d267c4dcc00b81c
hejiahao <hejiahao@intra.nsfocus.com>
2025-03-27 11:19:20 +0800

    optimize adjustMargins function

--- src/App.vue
+++ src/App.vue
@@ -14,22 +14,35 @@ const adjustMargins = () => {
 
     let currentPageStart = a4Container.offsetTop; // A4 当前页起点
 
-    elements.forEach((el) => {
+    elements.forEach((el, index) => {
+        let marginOffset = 0;
         const elTop = el.offsetTop; // 获取当前元素的顶部位置
+        const elBottom = Math.min(el.offsetTop + el.offsetHeight, elements[index + 1]?.offsetTop ?? Infinity);
+
+        // 计算相对于当前 A4 页面顶部的高度
+        const bottomlineHeight = PAGE_HEIGHT - BOTTOM_MARGIN; // 当前页面底部分隔线的高度
+        const elTopHeight = elTop - currentPageStart;
+        const elBottomHeight = elBottom - currentPageStart;
 
         // 检查是否跨页
-        if (elTop - currentPageStart > PAGE_HEIGHT - BOTTOM_MARGIN) {
-            // 计算 margin-top 让它对齐到下一个 A4 页面顶部边距
-            let marginOffset = currentPageStart + PAGE_HEIGHT - elTop + TOP_MARGIN;
-            // el.style.marginTop = `${marginOffset}px`
-            el.style.setProperty("--page-margin", `${marginOffset}px`);
-
-            // 进入下一页
+        if (elTopHeight > bottomlineHeight) {
+            // 上边界跨在分隔区：调整 margin-top 让它对齐到下一个 A4 页面顶部
+            marginOffset = BOTTOM_MARGIN + TOP_MARGIN - (elTopHeight - bottomlineHeight)
             currentPageStart += PAGE_HEIGHT;
-        } else {
-            // 保持默认值
-            el.style.setProperty("--page-margin", "0px");
+        } else if (elBottomHeight > bottomlineHeight) {
+            // 下边界跨在分隔区：判断是否真的需要分页
+            const topPartHeight = bottomlineHeight - elTopHeight;
+            const bottomPartHeight = elBottomHeight - bottomlineHeight;
+
+            // 只有当超出部分比留在当前页的部分大时或超出过多时，才换页
+            if (bottomPartHeight >= topPartHeight || bottomPartHeight >= BOTTOM_MARGIN / 2) {
+                marginOffset = bottomlineHeight - elTopHeight + BOTTOM_MARGIN + TOP_MARGIN
+                currentPageStart += PAGE_HEIGHT;
+            }
         }
+        // 避免 marginOffset 负值
+        marginOffset = Math.max(0, marginOffset);
+        el.style.setProperty("--page-margin", `${marginOffset}px`);
     });
 };
 
